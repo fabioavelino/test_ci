60852e7020f7f9179c0b878ccdad67ee2040b489
Fabio <fabio@MacBook-Air-de-Fabio.local>
2022-11-19 22:38:12 +0100

    fix

--- flutter-engine/install.sh
+++ flutter-engine/install.sh
@@ -2,6 +2,6 @@
 
 if [ `uname -m` == 'aarch64' ]; then export ARM=arm64; else export ARM=arm; fi
 
-install /home/runner/work/test_ci/test_ci/flutter_engine/$ARM/libflutter_engine.so.* /home/runner/work/test_ci/test_ci/flutter_engine/$ARM/icudtl.dat /usr/lib
-install /home/runner/work/test_ci/test_ci/flutter_engine/flutter_embedder.h /usr/include
+install ./flutter-engine/$ARM/libflutter_engine.so.* ./flutter-engine/$ARM/icudtl.dat /usr/lib
+install ./flutter-engine/flutter_embedder.h /usr/include
 
